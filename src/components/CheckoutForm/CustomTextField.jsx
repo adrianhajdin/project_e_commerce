ac84b6635096a5675e239c5713ed3d7ac4b90c80
Federico Lombardozzi <67126495+fedelombar@users.noreply.github.com>
2021-06-01 01:11:11 -0300

    FIX: STATUS 422  "The given data was invalid"
    A problem in Controller fixed.

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import { useFormContext, Controller } from 'react-hook-form';
-import { TextField, Grid } from '@material-ui/core';
+import React from "react";
+import { TextField, Grid } from "@material-ui/core";
+import { useFormContext, Controller } from "react-hook-form";
 
-function FormInput({ name, label, required }) {
+const FormInput = ({ name, label, required }) => {
   const { control } = useFormContext();
-  const isError = false;
 
   return (
     <Grid item xs={12} sm={6}>
       <Controller
-        as={TextField}
-        name={name}
+        defaultValue=""
         control={control}
-        label={label}
-        fullWidth
-        required={required}
-        error={isError}
+        name={name}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            name={name}
+            label={label}
+            required={required}
+            fullWidth
+          />
+        )}
       />
     </Grid>
   );
-}
+};
 
 export default FormInput;
